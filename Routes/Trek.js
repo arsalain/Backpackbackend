28992a84d8daddce7e58acbc8b938bf46ed3763c
unknown <arsalain12@gmail.com>
2023-09-25 21:48:48 +0530

    backend

---
+++ Routes/Trek.js
@@ -0,0 +1,25 @@
+import express from "express";
+import {
+    createTrek,
+    getTrekrecom,
+    getTreks,
+    getTreklong,
+    getTrekshort,
+    getTrekwaterfall,
+    getTrekById,
+    updateTrekById
+} from "../Controllers/trek.js"
+import {upload } from "../Middleware/upload.js"
+const router = express.Router();
+
+router.post("/createtrek",upload.fields([{name: 'testimage'}, { name: 'coverimage' }, {name : "planimg1"}, {name : "planimg2"}, {name : "planimg3"}, {name : "planimg4"}, {name : "planimg5"}, {name : "planimg6"}, {name : "planimg7"}, {name : "planimg8"} , {name : "planimg9"}, {name : "planimg10"},{name : "similarimg1"} , {name : "similarimg2"}, {name : "similarimg3"} ,{name : "reviewimg1"} , {name : "reviewimg2"}, {name : "reviewimg3"}   ] ),createTrek);
+router.put("/updatetrek/:id",upload.fields([{name: 'testimage'}, { name: 'coverimage' }, {name : "planimg1"}, {name : "planimg2"}, {name : "planimg3"}, {name : "planimg4"}, {name : "planimg5"}, {name : "planimg6"}, {name : "planimg7"}, {name : "planimg8"}, {name : "planimg9"}, {name : "planimg10"},{name : "similarimg1"} , {name : "similarimg2"}, {name : "similarimg3"},{name : "reviewimg1"} , {name : "reviewimg2"}, {name : "reviewimg3"}      ] ),updateTrekById);
+router.get("/get/:id",getTrekById)
+router.get("/gettreks",getTreks)
+router.get("/gettrekrecom",getTrekrecom)
+router.get("/gettreklong",getTreklong)
+router.get("/gettrekshort",getTrekshort)
+router.get("/gettrekwaterfall",getTrekwaterfall)
+
+export default router;
+
---
+++ index.js
@@ -0,0 +1,47 @@
+import express from "express"
+import dotenv from "dotenv"
+import mongoose from "mongoose"
+import TrekRoute from "./Routes/Trek.js"
+import TourRoute from "./Routes/Tour.js"
+import bodyParser from "body-parser";
+import cors from "cors";
+
+const app = express();
+app.use(cors({origin: 'http://localhost:3000'}));
+dotenv.config();
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log("Connected to Mongodb")
+    }catch(error){
+        throw error;
+    }
+};
+
+
+// app.use(cookieParser())
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json());
+
+app.use("/uploads",express.static('uploads'))
+app.use("/trek", TrekRoute);
+app.use("/tour",TourRoute)
+
+app.use((err, req, res, next) => {
+    const status = err.status || 500;
+    const message = err.message || "Something went wrong!";
+    return res.status(status).json({
+      success: false,
+      status,
+      message,
+    });
+  });
+
+app.listen(4000, ()=>{
+    connect();
+    console.log("Connected to backend at port 4000")
+})
+
+
+// const express = require("express")
